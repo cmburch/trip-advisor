99117817789fee7ea55797ba744a9ccc41fd57d7
Charles Burch <charles_burch89@yahoo.com>
2022-05-06 09:03:38 +0800

    starter code the maps component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+// import mapStyles from '../../mapStyles';
+import useStyles from './Map.styles.jsx';
 
 const Map = ()=> {
+    const matches = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+    const coordinates = { lat: 0, lng: 0 }
+ 
     return (
-        <h1>Map</h1>
-    )
+      <div className={classes.mapContainer}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          options={''}
+        >
+        </GoogleMapReact>
+      </div>
+    );
 }
 
 export default Map;
